728ed8b848acce1f772da62f5b28fcb67b1f3cc6
ABHISHEK-IITP <abhi9813293405@gmail.com>
2023-07-28 12:44:30 +0530

    message

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose'
 import cors from  'cors'
 import dotenv from "dotenv"
 
-import userRoutes from './routes/users.js'
-import questionRoutes from './routes/Questions.js'
-import answerRoutes from './routes/Answers.js'
+import userRoutes from "./routes/users.js";
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/Answers.js";
+import chatRoutes from "./routes/ChatAi.js";
+import socialRoutes from "./routes/Social.js";
+import subsRoutes from "./routes/subscription.js";
 // import connectDB from "./connectMongoDb.js"
 
 dotenv.config();
@@ -20,9 +23,13 @@ app.get('/', (req ,res)=>{
     res.send("This is stack overflow clone API")
 })
 
-app.use('/user',userRoutes)
-app.use('/question', questionRoutes)
-app.use('/answer', answerRoutes)
+app.use("/chat/", chatRoutes);
+app.use("/user/", userRoutes);
+app.use("/questions/", questionRoutes);
+app.use("/answer/", answerRoutes);
+app.use("/social/post/", socialRoutes);
+app.use("/subscription", subsRoutes);
+
 
 const PORT= process.env.PORT || 5000
 
@@ -31,5 +38,3 @@ const CONNECTION_URL= process.env.CONNECTION_URL
 mongoose.connect(CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT,()=> {console.log(`server running on port ${PORT}`)}))
 .catch((err)=> console.log(err.message))
-
-
